730555fc54921864d25a0e13d42fa33c7f47ed5a
Thane <thabay0606@students.maquoketaschools.org>
2026-05-08 17:46:20 +0000

    Maybe????

--- Classroom.cs
+++ Classroom.cs
@@ -3,6 +3,17 @@
 public class Classroom{
 
     public string FindMember(int memberIndex, string[] members){
+       
+       try
+        {
+            int length = memberIndex;
+            string member = members[100];
+        } catch (IndexOutOfRangeException indexOutOfRangeException)
+        {
+            return indexOutOfRangeException.Message;
+        }
+       
         return members[memberIndex];
+
     }
 }
